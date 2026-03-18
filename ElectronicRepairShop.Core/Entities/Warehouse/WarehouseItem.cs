820559e199aed179fe1dc6a1010db8eae7f786a5
The-Blight <hezer.fil@gmail.com>
2026-03-18 03:31:07 +0300

    Сделал класс WarehouseItem который является единицей товара в складе

--- ElectronicRepairShop.Core/Entities/Warehouse/WarehouseItem.cs
+++ ElectronicRepairShop.Core/Entities/Warehouse/WarehouseItem.cs
@@ -1,6 +1,22 @@
-﻿namespace ElectronicRepairShop.Core.Entities.Warehouse;
+﻿using ElectronicRepairShop.Core.Entities.Parts;
+
+namespace ElectronicRepairShop.Core.Entities.Warehouse;
+
+
+/// <summary>
+/// Класс представляет единицк товара на складе с учётом цен и текущего остатка.
+/// </summary>
 
 public class WarehouseItem
 {
+    public required Part Part { get; init; }
     
+    public decimal PurchasePrice { get; set; }
+    public decimal SellingPrice { get; init; }
+    public int QuantityInStock { get; set; }
+
+    /// <summary> 
+    /// Артикул запчасти. Возвращает значение из <see cref="Part.Article"/>. 
+    /// </summary>
+    public string Article => Part.Article;
 }
